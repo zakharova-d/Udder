55b078e33066d83bf2a67cc9dec93d6119a14ecc
Daria Zakharova <daria.zakharova.work@gmail.com>
2024-11-11 14:42:24 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Daria Zakharova on 11.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
